b62b922939e5361ac5f16d049d43ab222a0cd47b
Christopher Bell <christopherbell@outlook.com>
2024-07-19 19:15:20 +0100

    Add project files.

---
+++ Data/Tablefield.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorDapperCodeGenerator.Data
+{
+    public class Tablefield
+    {
+        // Auto-implemented properties.
+        public string Fieldname { get; set; } = String.Empty;
+        public string SQLtype { get; set; } = String.Empty;
+        public string Codetype { get; set; } = String.Empty;
+        public string DBtype { get; set; } = String.Empty;
+        public int Length { get; set; }
+        public bool IsRequired { get; set; }
+
+        public Tablefield()
+        {
+
+        }
+
+        public Tablefield(string fieldname, string sqltype, string codetype, string dbtype, int length, bool isrequired)
+        {
+            this.Fieldname = fieldname;
+            this.SQLtype = sqltype;
+            this.Codetype = codetype;
+            this.DBtype = dbtype;
+            this.Length = length;
+            this.IsRequired = isrequired;
+        }
+    }
+}
